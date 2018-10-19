bee59ffd8fa8ab70b49c6793b77295d601ea090a
Clint Kunz <clintkunz@gmail.com>
2018-10-19 10:32:27 -0600

    App component with nav and routes

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route } from 'react-router-dom';
+import Login from './components/Login.js';
+import Jokes from './components/Jokes.js';
+
+const Welcome = props => {
+  return (
+    <div>
+      <h3>Warning: Dad Jokes Ahead</h3>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <nav>
+            <NavLink exact to='/'>Warning</NavLink>
+            &nbsp; | &nbsp; 
+            <NavLink to='/login'>Login</NavLink>
+            &nbsp; | &nbsp; 
+            <NavLink to='/jokes'>Jokes</NavLink>
+            &nbsp; | &nbsp;
+            <NavLink to='/' onClick={this.signout}>Logout</NavLink>
+          </nav>
+          <main>
+            <Route exact path='/' component={Welcome} />
+            <Route path='/login' component={Login} />
+            <Route path='/jokes' component={Jokes} />
+          </main>
         </header>
       </div>
     );
   }
+
+  signout = () => {
+    localStorage.removeItem('jwt');
+  };
 }
+
+
 
 export default App;
